054fc9a2a56f3a84154afadb8ae7fbbbcda7f73e
Kae Anderson <andersonkaem@gmail.com>
2017-03-15 15:36:56 -0500

    started HTML

---
+++ app/routes/application.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <div class="app">
+    <div class="nav">
+      <div class="left">
+        <div class="left__words">Ribbiter</div>
+      </div>
+      <div class="right">
+        <div class="right__words">Ribbits</div>
+        <div class="right__words">Frogs</div>
+        <div class="right__words">Login</div>
+      </div>
+    </div>
+
+  <div class="section">
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
