0b61cb7f984b0890fc3c45f2ae29f6bcb4a77c75
Kae Anderson <andersonkaem@gmail.com>
2017-03-15 16:30:27 -0500

    styled index

--- app/routes/index.vue
+++ app/routes/index.vue
@@ -1,6 +1,14 @@
 <template lang="html">
-  <div class="">
-
+  <div class="appindex">
+    <div class="card">
+      <h2>Welcome to Ribitter!</h2>
+        <div class="buttons">
+          <!--<router-link class="nav-item button" :to="{ name: 'register' }">Register</router-link>-->
+          <button class="button">Register</button>
+          <!--<router-link class="nav-item button" :to="{ name: 'login' }">Login</router-link>-->
+          <button class="button">Login</button>
+        </div>
+      </div>
   </div>
 </template>
 
